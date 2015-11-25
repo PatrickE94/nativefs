7c6c43ca56244c23f87d9a3983b67c6cad8f4de6
Patrick Engström <patrick@weblab.se>
2015-11-25 23:56:59 +0100

    Added simple wrapper for move. First copies then unlinks source.

--- nativefs.js
+++ nativefs.js
@@ -2,10 +2,29 @@
   "use strict";
 
   var native_fs = require('./build/Release/native_fs');
+  var fs = require('fs');
 
   module.exports = {};
   module.exports.copy = function() {
     return native_fs.copy.apply(null, arguments);
+  };
+
+  module.exports.move = function(pathone, pathtwo, progcb, rescb) {
+    if (rescb === undefined) {
+      rescb = progcb;
+      progcb = null;
+    }
+
+    var tmpcb = function(err, res) {
+      if (err) return rescb(err, res);
+      fs.unlink(pathone, function(err) {
+        if (err) return rescb(err, false);
+        rescb(null, true);
+      });
+    };
+
+    if (progcb) native_fs.copy.call(null, pathone, pathtwo, progcb, tmpcb);
+    else native_fs.copy.call(null, pathone, pathtwo, tmpcb);
   };
 
   return;
